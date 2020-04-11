32bbaedcb4728f00e499f2508dcc9689cb3a8b9c
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-04-11 19:18:39 +0300

    Super simple battle system.

---
+++ Assets/Battle/BattleController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleController : MonoBehaviour
+{
+    private int enemyHitPoints = 100;
+    public int playerHitPoints = 100;
+
+    [NonSerialized]
+    public bool isPlayerTurn;
+    
+    private bool isAwaitingUserInput;
+
+    private bool isBattleInProgress
+    {
+        get { return playerHitPoints > 0 && enemyHitPoints > 0; }
+    }
+    
+    void Start()
+    {
+        StartCoroutine(BattleCoroutine());
+    }
+    
+    public void OnAttackPressed()
+    {
+        isAwaitingUserInput = false;
+    }
+
+    private IEnumerator BattleCoroutine()
+    {
+        while (isBattleInProgress)
+        {
+            yield return PlayerTurn();
+            yield return EnemyTurn();
+        }
+    }
+    
+    private IEnumerator PlayerTurn()
+    {
+        isPlayerTurn = true;
+        
+        isAwaitingUserInput = true;
+        
+        while (isAwaitingUserInput)
+        {
+            yield return null;
+        }
+
+        enemyHitPoints -= 10;
+        
+        isPlayerTurn = false;
+    }
+
+    private IEnumerator EnemyTurn()
+    {
+        playerHitPoints -= 1;
+        
+        yield return null;
+    }
+}
---
+++ Assets/Battle/BattleInterfaceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BattleInterfaceController : MonoBehaviour
+{
+    public BattleController battleController;
+    
+    public TMP_Text textHitPoints;
+    
+    public GameObject buttonAttack;
+
+    void Awake()
+    {
+        buttonAttack.SetActive(false);
+    }
+    
+    void Update()
+    {
+        textHitPoints.text = battleController.playerHitPoints.ToString();
+        buttonAttack.SetActive(battleController.isPlayerTurn);
+    }
+}
