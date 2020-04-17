de59f0a5ede9c004d4228116739afd8a4562724d
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-04-18 00:07:30 +0300

    UI progress.

---
+++ Assets/Common/Scripts/UI/InventoryItemsList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemsList : MonoBehaviour
+{
+    public GameObject itemListPrefab;
+        
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject gameObjectListItem = Instantiate(itemListPrefab);
+            gameObjectListItem.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
